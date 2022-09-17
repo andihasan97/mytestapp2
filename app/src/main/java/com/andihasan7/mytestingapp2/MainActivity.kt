c28f7beec5cbe4f2c5bd7bc1ea0bb5767a7d2583
andihasan97 <andihasanelfalakiy@gmail.com>
2022-09-17 07:51:08 +0700

    mencoba menampilkan exception OutOfBound

--- app/src/main/java/com/andihasan7/mytestingapp2/MainActivity.kt
+++ app/src/main/java/com/andihasan7/mytestingapp2/MainActivity.kt
@@ -2,25 +2,42 @@ package com.andihasan7.mytestingapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private var btnSetValue: Button? = null
+
+    private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnSetValue = findViewById(R.id.btn_set_value)
         tvText = findViewById(R.id.tv_text)
-        btnSetValue!!.setOnClickListener(this)
+
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Naerendra Wicaksono")
+        names.add("Andi")
+        names.add("Hasan")
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.btn_set_value) {
-            tvText.text = "19"
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            // i in 0..3 akan tampil exception OutOfBound karena index maksimal 2 size 3
+            // ganti ke i in 0..2 untuk solved
+            for (i in 0..3) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
         }
     }
 }
